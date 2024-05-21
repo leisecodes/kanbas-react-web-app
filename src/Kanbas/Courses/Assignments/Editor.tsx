f337f74c140111f37b69742d61bef33fdb3c2b2c
lcran <leise.crandall@jax.org>
2024-05-21 13:14:36 -0400

    styling edits

--- src/Kanbas/Courses/Assignments/Editor.tsx
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -1,3 +1,5 @@
+
+
 export default function AssignmentEditor() {
     return (
       <div id="wd-assignments-editor">
@@ -23,21 +25,20 @@ export default function AssignmentEditor() {
         </textarea>
         <br />
         <br/>
-        <table>
           <div className="row">
-            <div  className="col">
+            <div  className="col-3 text-end">
               <label htmlFor="wd-points" className="form-label">Points</label>
             </div>
-            <div className="col">
+            <div className="col-5">
               <input className="form-control" id="wd-points" value={100} />
             </div>
           </div>
           <br/>
           <div className="row">
-            <div  className="col">
+            <div  className="col-3 text-end">
                 <label htmlFor="wd-group" className="form-label">Assignment Group</label>
             </div>
-            <div  className="col">
+            <div  className="col-5">
                 <select className="form-select" id="wd-group">
                     <option value="Assignments">ASSIGNMENTS</option>
                     <option value="Quizzes">QUIZZES</option>
@@ -47,10 +48,10 @@ export default function AssignmentEditor() {
           </div>
           <br/>
           <div className="row">
-          <div  className="col">
+          <div  className="col-3 text-end">
                 <label htmlFor="wd-display-grade-as" className="form-label">Display Grade As</label>
             </div>
-            <div  className="col">
+            <div  className="col-5">
                 <select className="form-select" id="wd-display-grade-as">
                     <option value="Percentage">Percentage</option>
                 </select>
@@ -58,77 +59,69 @@ export default function AssignmentEditor() {
           </div>
           <br/>
           
-          <div className="row d-flex">
-            <div className="col">
+          <div className="row">
+            <div className="col-3 text-end">
                 <label htmlFor="wd-submission-type" className="form-label">Submission Type</label>
             </div>
-            <div className="border border-dark rounded-1">
-            <div className="col">
-                <select className="form-select" id="wd-submission-type">
+            
+            <div className="col-5 ms-3 border border-grey p-3 rounded-1">
+                <select className="form-select w-100" id="wd-submission-type">
                     <option value="Online">Online</option>
                 </select>
           
               <br/>
-                Online Entry Options <br/>
+                <div className="">Online Entry Options</div> <br/>
                 <div className="form-check">
                 <input className="form-check-input" type="checkbox" name="wd-text-entry" id="wd-text-entry"></input>
-                <label htmlFor="wd-text-entry" className="form-check-label">Text Entry</label>
+                <label htmlFor="wd-text-entry" className="form-check-label pb-2 ">Text Entry</label>
                 <br/>
                 <input className="form-check-input" type="checkbox" name="wd-website-url" id="wd-website-url"></input>
-                <label htmlFor="wd-website-url" className="form-check-label">Website URL</label>
+                <label htmlFor="wd-website-url" className="form-check-label pb-2">Website URL</label>
                 <br/>
                 <input className="form-check-input" type="checkbox" name="wd-media-recordings" id="wd-media-recordings"></input>
-                <label htmlFor="wd-media-recordings" className="form-check-label">Media Recordings</label>
+                <label htmlFor="wd-media-recordings" className="form-check-label pb-2">Media Recordings</label>
                 <br/>
                 <input className="form-check-input" type="checkbox" name="wd-student-annotation" id="wd-student-annotation"></input>
-                <label htmlFor="wd-student-annotation" className="form-check-label">Student Annotation</label>
+                <label htmlFor="wd-student-annotation" className="form-check-label pb-2">Student Annotation</label>
                 <br/>
                 <input className="form-check-input" type="checkbox" name="wd-file-upload" id="wd-file-upload"></input>
-                <label htmlFor="wd-file-upload" className="form-check-label">File Uploads</label>
+                <label htmlFor="wd-file-upload" className="form-check-label pb-2">File Uploads</label>
                 </div>
           </div>
           </div>
-          </div>
           <br/>
           <div className="row">
-          <div  className="col">Assign</div>
-          <div  className="col">
+          <div  className="col-3 text-end">Assign</div>
+          <div  className="col-5  ms-3 border border-grey p-3 rounded-1 mb-3">
                 <label htmlFor="wd-assign-to" className="form-label">Assign To</label><br/>
-                <input className="form-control" type="text" name="wd-assign-to" id="wd-assign-to" value="Everyone"></input>
-            </div>
-          </div>
-          <div className="row">
-          <div  className="col"></div>
-          <div  className="col">
+                <input className="form-control mb-3" type="text" name="wd-assign-to" id="wd-assign-to" value="Everyone"></input>
                 <label htmlFor="wd-due-date" className="form-label">Due</label><br/>
-                <input className="form-control" type="date" id="wd-due-date"></input>
-            </div>
-          </div>
-          <br/>
-          <div className="row">
-          <div  className="col"></div>
-          <div  className="col">
+                <input className="form-control mb-3" type="date" id="wd-due-date"></input>
+                <div className="row">
+                <div className="col-6">
                 <label htmlFor="wd-available-from" className="form-label">Available From</label><br/>
-                <input className="form-control" type="date" id="wd-available-from"></input>
-            </div>
-            <div  className="col">
+                <input className="form-control " type="date" id="wd-available-from"></input>
+                </div>
+                <div className="col-6">
                 <label htmlFor="wd-available-until" className="form-label">Until</label><br/>
                 <input className="form-control" type="date" id="wd-available-until"></input>
+                </div>
+                </div>
             </div>
           </div>
-          <br/>
+          <div  className="col-3 text-end"></div>
+          <div  className="col-5">
+                
+            </div>
           <div className="row">
           <div  className="col"><hr/></div>
             </div>
-            <div className="row">
-            <div  className="col"></div>
-            <div  className="col"></div>
-            <div  className="col d-flex">
-                <button className="btn btn-secondary">Cancel</button>
+            <div className="row w-75 ">
+            <div  className="d-flex justify-content-end mb-5">
+                <button className="btn btn-secondary me-1">Cancel</button>
                 <button className="btn btn-danger">Save</button>
                 </div>
             </div>
-        </table>
       </div>
     );
   }
--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -3,11 +3,15 @@ import Modules from "./Modules";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
 import Home from "./Home";
+import { FaBars } from "react-icons/fa6";
 import { Navigate, Route, Routes } from "react-router";
 export default function Courses() {
   return (
     <div id="wd-courses">
+      <div className="d-flex">
+      <FaBars className="me-4 align-self-center text-danger"/>
       <h2>Course 1234</h2>
+      </div>
       <hr />
       <div className="d-flex">
         <div className="d-none d-md-block">
